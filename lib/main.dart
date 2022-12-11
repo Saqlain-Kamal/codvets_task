6674af800b9f78d4f5ba782ac38304152ca4510a
kamal khan <kamal10832@gmail.comn>
2022-12-11 09:56:11 +0500

    add all once again

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black,
         body: Column(
           children: [
             Padding(
